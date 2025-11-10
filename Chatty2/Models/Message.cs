2f4eeef964530d5f1ef905004fcac39e7382f1f7
uwr59 <uwrwork@outlook.com>
2025-11-10 14:54:51 +0000

    basic messageboard

---
+++ Chatty2/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Chatty2.Models
+{
+    // Models/Message.cs
+    public class Message
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsPinned { get; set; } = false;
+    }
+}
